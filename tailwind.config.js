645db59bce2b415cc2d5867ada516636a4cd173d
George Calder <georgecalder@Georges-MacBook-Air.local>
2025-10-15 20:16:30 +0100

    Add dark mode styles and Tailwind updates

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.mjs
+export default {
+  content: [
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./src/app/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: { extend: {} },
+  plugins: [],
+};
