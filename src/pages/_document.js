1e82c9643c5d7a555a7fac61b610d5108ade4d01
George Calder <georgecalder@Georges-MacBook-Air.local>
2025-11-04 10:10:01 +0000

    fix: update _document to reduce flash

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,11 +1,10 @@
-/* eslint-disable @next/next/no-sync-scripts */
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head>{/* no custom <script> tags here */}</Head>
-      <body className="antialiased">
+    <Html>
+      <Head />
+      <body>
         <Main />
         <NextScript />
       </body>
