1efad2920591d8a7dc52841832fe366afb035939
George Calder <georgecalder@Georges-MacBook-Air.local>
2025-11-04 10:13:50 +0000

    revert: restore stable _app and _document to fix layout

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,14 @@
+// src/pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        {/* If you truly need a script before hydration, uncomment and point to it */}
+        {/* <Script src="/path/to/your-script.js" strategy="beforeInteractive" /> */}
+      </Head>
       <body>
         <Main />
         <NextScript />
